f1b2e6c6ba8bcb6029a195dc51f55f6bf65b2a5d
tourn <latzer.daniel@gmail.com>
2017-01-04 14:03:34 +0100

    MAKE LOVE MORE GENTLY

--- animations/love.js
+++ animations/love.js
@@ -25,24 +25,134 @@ const love = [
     "(\\_/)(^_^)",
     "(^_^)<) <)",
   ]},
-  { loop: 4, frames: [
-    { duration: duration, image: [
-      "    (\\_/)",
-      "(\\_/)^_^)",
-      "(^_^)<)<)",
-    ]},
-    { duration: duration, image: [
-      "    (\\_/)",
-      "(\\_/)^_^)",
-      "(^_^)<) <)",
-    ]}
+  // --------------------
+  { duration: duration * 10, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
   ]},
+  { duration: duration * 10, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 9, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 9, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 8, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 8, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 7, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 7, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 6, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 6, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 5, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 5, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 4, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 4, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 3, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 3, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 2, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 2, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 1, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 1, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 1, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 1, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  { duration: duration * 1, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<)<)",
+  ]},
+  { duration: duration * 1, image: [
+    "    (\\_/)",
+    "(\\_/)^_^)",
+    "(^_^)<) <)",
+  ]},
+  // ---------------------
   { duration: duration * 5, image: [
     "    (\\_/)",
     "(\\_/)^_^)",
     "(O_O)<)<)",
   ]},
-  { duration: duration * 5, image: [
+  { duration: duration * 15, image: [
     "<3  (\\_/)",
     "(\\_/)^_^)",
     "(^_^)<)<)",
