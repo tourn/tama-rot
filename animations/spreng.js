cb12ff6340548236810a5f22d65a4951e2e96579
tourn <latzer.daniel@gmail.com>
2017-01-04 14:14:01 +0100

    BLAST THE HANS TO SMITHEREENS

---
+++ animations/spreng.js
@@ -0,0 +1,73 @@
+const spreng_duration = 300;
+const spreng = [
+  { duration: spreng_duration, image: [
+    " (\\_/)",
+    " (O_o)",
+    "|||||||",
+  ]},
+
+  { loop: 3, frames: [
+    { duration: spreng_duration, image: [
+      " (\\_/)",
+      " (_)",
+      "|||||||",
+    ]},
+
+    { duration: spreng_duration, image: [
+      " (\\_/)",
+      " (^_^)",
+      "|||||||",
+    ]},
+  ]},
+
+  { duration: spreng_duration * 3, image: [
+    " (\\_/) {BUMM}",
+    " (^_^)/",
+    "|||||||",
+  ]},
+
+  /*
+  { duration: spreng_duration, image: [
+    " (\\_/) {?}",
+    " (?_?)/",
+    "|||||||",
+  ]},
+  { duration: spreng_duration, image: [
+    " (\\_/)",
+    " (-.-)",
+    "|||||||",
+  happy-1happy
+  ]},
+  Else
+
+  */
+  { duration: spreng_duration, image: [
+    " (\\ /)",
+    "(x   x)",
+    "|| _ ||_",
+    "*Explosion*",
+  ]},
+  { duration: spreng_duration, image: [
+    "(     )",
+    "x\\   /x)",
+    "|  _  |__",
+    "*Explosion*",
+  ]},
+  { duration: spreng_duration, image: [
+    "         ",
+    "        /",
+    "   _   __x)",
+    "*Explosion*",
+  ]},
+  { duration: spreng_duration, image: [
+    "         ",
+    "        ",
+    "   _   __   x)",
+    "*Explosion*",
+  ]},
+  { duration: spreng_duration, image: [
+    "         ",
+    "        ",
+    "   _   __   x)",
+  ]},
+];
