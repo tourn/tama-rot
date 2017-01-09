9827d39d9e16db27eadd835b09b82b058d7322fc
blubiszirp <frank-jordi@hotmail.com>
2017-01-09 13:56:31 +0100

    HANS WONT STOP STARING AT YOU

---
+++ scripts/animations/idle.js
@@ -0,0 +1,21 @@
+define(function(){
+const duration = 100;
+return [
+	{
+        duration: duration * 50,
+        image: [
+			"(\\_/)",
+			"(O_o)",
+			"(> <)"
+        ]
+    },
+    {
+        duration: duration,
+        image: [
+			"(\\_/)",
+			"(-_-)",
+			"(> <)"
+        ]
+    },
+]
+});
