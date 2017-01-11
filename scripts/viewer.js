0ae70368c9b4da7613e5d6cf7fe71d07fe75a6be
tourn <latzer.daniel@gmail.com>
2017-01-11 14:33:16 +0100

    Make viewer loop animations

--- scripts/viewer.js
+++ scripts/viewer.js
@@ -8,12 +8,18 @@ requirejs.config({
 
 requirejs(['render', 'animations/animations'], function(render, animations){
   const list = document.getElementById('animations');
+  list.innerHTML += "<option></option>";
   Object.keys(animations).forEach(function(name){
     console.log(name);
     list.innerHTML += "<option>"+name+"</option>";
   });
 
   list.onchange = function(){
-    render.animate(this.value);
+    animate(this.value);
   };
+
+  function animate(name){
+    render.clearAnimation();
+    render.animate(name).then(function(again){ if(again) {animate(name)} });
+  }
 });
