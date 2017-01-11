ff30e11b41b3055a1797dd9bf772817b49fe3463
tourn <latzer.daniel@gmail.com>
2017-01-11 14:15:42 +0100

    Copy viewer from animations branch

---
+++ scripts/viewer.js
@@ -0,0 +1,19 @@
+requirejs.config({
+  shim: {
+    'rot': {
+      exports: 'ROT'
+    }
+  }
+});
+
+requirejs(['render', 'animations/animations'], function(render, animations){
+  const list = document.getElementById('animations');
+  Object.keys(animations).forEach(function(name){
+    console.log(name);
+    list.innerHTML += "<option>"+name+"</option>";
+  });
+
+  list.onchange = function(){
+    render.animate(this.value);
+  };
+});
