fe78f17da2dfb0ce11afbd2e8256368a926e4a4e
blubiszirp <frank-jordi@hotmail.com>
2017-01-11 11:14:59 +0100

    I AM SICK OF MY SHIT

---
+++ scripts/animations/idle4.js
@@ -0,0 +1,134 @@
+define(function(){
+// Sick - ueberfressen
+const duration = 100;
+return [
+	{
+        duration: duration * 20,
+        image: [
+			"(\\_/)",
+			"(O_o)",
+			"(> <)"
+        ]
+    },
+	{
+        duration: duration * 10,
+        image: [
+			"(\\_/)",
+			"( O_)",
+			"(> <)"
+        ]
+    },
+	{
+        duration: duration * 10,
+        image: [
+			"  /) ",
+			" ( O)",
+			"o( >)"
+        ]
+    },
+	{
+        duration: duration,
+        image: [
+			"  /) ",
+			" ( X)",
+			"o( >)"
+        ]
+    },
+	{
+        duration: duration,
+        image: [
+			"  /) ",
+			" ( X)-",
+			"o( >)"
+        ]
+    },
+	{
+        duration: duration,
+        image: [
+			"  /) ",
+			" ( X)-",
+			"o( >)\\"
+        ]
+    },
+	{
+        duration: duration,
+        image: [
+			"  /) ",
+			" ( X)-__",
+			"o( >)_\\"
+        ]
+    },
+	{
+        duration: duration,
+        image: [
+			"  /) ",
+			" ( X)-__",
+			"o( >)_\\_"
+        ]
+    },
+	{
+        duration: duration,
+        image: [
+			"  /) ",
+			" ( X)-__",
+			"o( >)_\\\\_"
+        ]
+    },
+	{
+        duration: duration,
+        image: [
+			"  /) ",
+			" ( X)-__",
+			"o( >)_\\\\__"
+        ]
+    },
+	{
+        duration: duration,
+        image: [
+			"  /) ",
+			" ( X) __",
+			"o( >)_\\\\__"
+        ]
+    },
+	{
+        duration: duration,
+        image: [
+			"  /) ",
+			" ( X) ",
+			"o( >)_\\\\__"
+        ]
+    },
+	{
+        duration: duration*10,
+        image: [
+			"  /) ",
+			" ( X) ",
+			"o( >)_▲▲___"
+        ]
+    },
+	{
+        duration: duration*10,
+        image: [
+			"(\\_/)",
+			"( X_) ",
+			"(> <)_▲▲___"
+        ]
+    },
+	{
+        duration: duration*10,
+        image: [
+			"(\\_/)",
+			"(X_x) ",
+			"(> <)_▲▲___"
+        ]
+    },
+	{
+        duration: duration*20,
+        image: [
+			"(\\_/)",
+			"(O_o) ",
+			"(> <)_▲▲___"
+        ]
+    },
+]
+});
