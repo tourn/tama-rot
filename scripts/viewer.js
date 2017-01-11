84622f2eec89f979029de0e3f7886e5f10f265b8
tourn <latzer.daniel@gmail.com>
2017-01-11 14:36:40 +0100

    Make viewer remember last played animation

--- scripts/viewer.js
+++ scripts/viewer.js
@@ -10,16 +10,20 @@ requirejs(['render', 'animations/animations'], function(render, animations){
   const list = document.getElementById('animations');
   list.innerHTML += "<option></option>";
   Object.keys(animations).forEach(function(name){
-    console.log(name);
     list.innerHTML += "<option>"+name+"</option>";
   });
 
   list.onchange = function(){
+    window.sessionStorage.animation = this.value;
     animate(this.value);
   };
 
   function animate(name){
     render.clearAnimation();
     render.animate(name).then(function(again){ if(again) {animate(name)} });
+  }
+
+  if(window.sessionStorage.animation){
+    animate(window.sessionStorage.animation);
   }
 });
