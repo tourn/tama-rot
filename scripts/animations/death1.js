a89d522e83c6ac1129f2d5c3014185e53043db2c
blubiszirp <frank-jordi@hotmail.com>
2017-01-11 14:05:00 +0100

    RIP

---
+++ scripts/animations/death1.js
@@ -0,0 +1,15 @@
+define(function(){
+const duration = 5000;
+return [
+	{
+        duration: duration,
+        image: [
+			"        ___",
+			"       /   \\",
+			"       |RIP|",
+			"       |   |",
+			"^^^^^^^=====^^^^^^^^"
+        ]
+    },
+]
+});
