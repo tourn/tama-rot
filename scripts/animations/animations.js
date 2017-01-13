c38be7f20c902f959d908c94c45d623016f0fed1
tourn <latzer.daniel@gmail.com>
2017-01-13 14:08:10 +0100

    Rewrite antimations defintions file for less redundancy

--- scripts/animations/animations.js
+++ scripts/animations/animations.js
@@ -1,97 +1,62 @@
-define([
-  './beat',
-  './cuddle',
-  './death1',
-  './death2',
-  './death3',
-  './deathwall',
-  './defi',
-  './eat',
-  './feed',
-  './happy1',
-  './idle1',
-  './idle2',
-  './idle3',
-  './idle4',
-  './love',
-  './music',
-	'./overeatenexplode',
-  './play',
-  './sad1',
-  './sad2',
-  './sad3',
-  './sleep',
-  './spreng',
-  './suicide',
-  './todo',
-  './train',
-  './training',
-], function(
-  beat,
-  cuddle,
-  death1,
-  death2,
-  death3,
-  deathwall,
-  defi,
-  eat,
-  feed,
-  happy1,
-  idle1,
-  idle2,
-  idle3,
-  idle4,
-  love,
-  music,
-	overeatenexplode,
-  play,
-  sad1,
-  sad2,
-  sad3,
-  sleep,
-  spreng,
-  suicide,
-  todo,
-  train,
-  training
-){
-  return {
-    beat: beat,
-    cuddle: cuddle,
-    death: ["wrandom", [10, death1], [10, death2], [1, death3]],
-    death1: death1,
-    death2: death2,
-    death3: death3,
-    deathwall: deathwall,
-    defi: defi,
-    eat: eat,
-    feed: feed,
-		happy1,
-    idle: ["wrandom", [10, idle1], [10, idle2], [2, idle3]],
-    idle1: idle1,
-    idle2: idle2,
-    idle3: idle3,
-    idle4: idle4, //TODO rename
-    love: love,
-    music: music,
-		overeatenexplode:  overeatenexplode,
-    play: play,
-    sad1: sad1,
-    sad2: sad2,
-    sad3: sad3,
-    sleep: sleep,
-    spreng: spreng,
-    suicide: suicide,
-    train: train,
-    training: training,
-    missing: train,
-    todo: todo,
-    TODO: todo,
-    stat_satiety_low: ["wrandom", [10, todo]],
-    stat_satiety_high: ["wrandom", [10, idle4]],
-    stat_energy_low: ["wrandom", [10, todo]],
-    stat_energy_high: ["wrandom", [10, todo]],
-    stat_happy_low: ["wrandom", [10, sad1], [5, sad2], [1, sad3]],
-    stat_happy_high: ["wrandom", [10, happy1]],
+const animations = [
+  'beat',
+  'cuddle',
+  'death1',
+  'death2',
+  'death3',
+  'deathwall',
+  'defi',
+  'eat',
+  'feed',
+  'happy1',
+  'idle1',
+  'idle2',
+  'idle3',
+  'idle4', //TODO: rename
+  'love',
+  'music',
+  'overeatenexplode',
+  'play',
+  'sad1',
+  'sad2',
+  'sad3',
+  'sleep',
+  'spreng',
+  'suicide',
+  'todo',
+  'train',
+  'training',
+];
+define(animations.map(function(path){ return './' + path;}), function(){
+  var exports = {};
+  for(var i=0; i < animations.length; i++){
+    const name = animations[i];
+    exports[name] = arguments[i];
   }
+  const TODO = exports['todo'];
+
+  function multi(choices){
+    return ['wrandom'].concat(choices.map(function(choice){
+      const def = exports[choice[1]];
+      if(!def){
+        console.log("Animation not found: " + choice[1]);
+        def = TODO;
+      }
+      return [choice[0], def];
+    }));
+  }
+
+  return Object.assign(exports, {
+    death: multi([[10, "death1"], [10, "death2"], [1, "death3"]]),
+    idle: multi([[10, "idle1"], [10, "idle2"], [2, "idle3"]]),
+    TODO: TODO,
+    stat_satiety_low: TODO,
+    stat_satiety_high: multi([[10, "idle4"]]),
+    stat_energy_low: TODO,
+    stat_energy_high: TODO,
+    stat_happy_low: multi([[10, "sad1"], [5, "sad2"], [1, "sad3"]]),
+    stat_happy_high: multi([[10, "happy1"]]),
+  });
+
 });
+
