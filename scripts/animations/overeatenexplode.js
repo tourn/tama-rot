4e2f3e4d242845dbb861201efb010da05b43a65f
blubiszirp <frank-jordi@hotmail.com>
2017-01-13 13:47:13 +0100

    IM FULL ... OH, A CAKE ...

---
+++ scripts/animations/overeatenexplode.js
@@ -0,0 +1,399 @@
+define(function(){
+const duration = 100;
+return [
+	{
+    duration: duration * 10,
+    image: [
+			"(\\__/)         _",
+			"(o__o)       _|_|_",
+			"(>  <)      |^^^^^|"
+    ]
+  },
+  {
+    duration: duration * 10,
+    image: [
+			"(\\__/)         _",
+			"( o__)       _|_|_",
+			"(>  <)      |^^^^^|"
+    ]
+  },
+	{
+    duration: duration,
+    image: [
+			"(\\__/)         _",
+			"( -__)       _|_|_",
+			"(>  <)      |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 10,
+    image: [
+			"(\\__/)         _",
+			"( O__)       _|_|_",
+			"(>  <)      |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 10,
+    image: [
+			"(\\__/)         _",
+			"( ^__)       _|_|_",
+			"(>  <)      |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"(\\__/)         _",
+			"(  ^_)       _|_|_",
+			"( >  >      |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"   /)          _",
+			"(   ^)       _|_|_",
+			"(  > )      |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"    /)         _",
+			" (   ^)      _|_|_",
+			"O(  > )     |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"     /)        _",
+			"  (   ^)     _|_|_",
+			" O(  > )    |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"      /)       _",
+			"   (   ^)    _|_|_",
+			"  O(  > )   |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"       /)      _",
+			"    (   ^)   _|_|_",
+			"   O(  > )  |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"        /)     _",
+			"     (   ^)  _|_|_",
+			"    O(  > ) |^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"         /)    _",
+			"      (   ^) _|_|_",
+			"     O(  > )|^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 3,
+    image: [
+			"         /)    _",
+			"      (   ^)  |_|_",
+			"     O(   >)|^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 3,
+    image: [
+			"         /)    _",
+			"      (   ^) _|_|_",
+			"     O(   >)|^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 10,
+    image: [
+			"         /)    _",
+			"      (   ^) _|_|_",
+			"     O(    >|^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"         /)   _",
+			"      (   ^)_|_|_",
+			"     O(   >|^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"         /)  _",
+			"      (   ^)|_|_",
+			"     O(  >|^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"         /) _",
+			"      (   ^|_|_",
+			"     O(  >^^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"         /)_",
+			"      (   ^)|_",
+			"     O(  >^^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"         /)",
+			"      (   ^|_",
+			"     O(  >^^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"         /)",
+			"      (   ^)",
+			"     O(  >^^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"         /)",
+			"      (   ^)",
+			"     O(  >^|"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"         /)",
+			"      (   ^)",
+			"     O(  >|)"
+    ]
+  },
+	{
+    duration: duration * 10,
+    image: [
+			"         /)",
+			"      (   ^)",
+			"     O(  > )"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"        /)/)",
+			"      (  ^_)",
+			"     O(  > )"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"       /)/)",
+			"     (  ^_)",
+			"    O(  > )"
+    ]
+  },
+	{
+    duration: duration * 5,
+    image: [
+			"      ()_/)",
+			"     ( ^__)",
+			"    O( >  >"
+    ]
+  },
+	{
+    duration: duration * 10,
+    image: [
+			"    (\\___/)",
+			"    (^___^)",
+			"    (>   <)"
+    ]
+  },
+	{
+    duration: duration * 3,
+    image: [
+			"    (\\___/)",
+			"    (O___O)",
+			"    (>   <)"
+    ]
+  },
+	{
+    loop: 5,
+    frames: [
+		{
+			duration: duration * 2,
+			image: [
+				"    (\\___/)",
+				"    (O___O)",
+				"    (>   <)"
+			]
+		},
+		{
+			duration: duration * 2,
+			image: [
+				"   (\\_____/)",
+				"   (O_____O)",
+				"   (>     <)"
+			]
+		},
+		]
+	},
+	{
+    loop: 5,
+    frames: [
+		{
+			duration: duration * 2,
+			image: [
+				"    (\\___/)",
+				"    (>___<)",
+				"    (>   <)"
+			]
+		},
+		{
+			duration: duration * 2,
+			image: [
+				"   (\\_____/)",
+				"   (>_____<)",
+				"   (>     <)"
+			]
+		},
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"   /|__ __|\\",
+			"   (>__-__<)",
+			"   (>  -  <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"    /|___|\\",
+			"   (X__-__X)",
+			"  (>       <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"   (X__ __X)",
+			"   (__   __)",
+			" (>         <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"   (X_   _X)",
+			"  (__     __)",
+			"(>           <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"   (X_   _X)",
+			"  (__     __)",
+			">             <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"   (X-   -X)",
+			"            ",
+			">(--       --) <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"   (X-   -X)",
+			"            ",
+			" (__       __)  <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"  (X-     -X)",
+			"            ",
+			" (__       __)  <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"    _     _",
+			"  (X       X)",
+			" (__       __)  <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"",
+			"  (X-     -X)",
+			" (__       __)  <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"",
+			"  (X-     -X)",
+			"(__        __)   <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"",
+			"    _     _",
+			"(_(X       X)_)   <)"
+		]
+	},
+	{
+		duration: duration,
+		image: [
+			"",
+			"",
+			"___X-     -X__)   <)"
+		]
+	},
+	{
+		duration: duration*10,
+		image: [
+			"",
+			"",
+			"___X_     _X__)   <)"
+		]
+	},
+]
+});
