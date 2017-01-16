f2dd16200211de81cd8d93beb45d6d75027c18ce
blubiszirp <frank-jordi@hotmail.com>
2017-01-16 10:09:58 +0100

    HANS CAN BE HUNGRY

---
+++ scripts/animations/hungry1.js
@@ -0,0 +1,37 @@
+define(function(){
+const duration = 100;
+return [
+	{
+    duration: duration * 30,
+    image: [
+			"(\\/)",
+			"(Oo)",
+			"(><)"
+    ]
+  },
+	{
+    duration: duration,
+    image: [
+			"(\\/)",
+			"(--)",
+			"(><)"
+    ]
+  },
+	{
+    duration: duration * 10,
+    image: [
+			"(\\/)",
+			"(Oo)",
+			"(><)"
+    ]
+  },
+  {
+    duration: duration*30,
+    image: [
+			"(\\/) NOM?",
+			"(Oo)/",
+			"(><)"
+    ]
+  },
+]
+});
