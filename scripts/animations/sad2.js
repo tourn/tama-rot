3086e44a21d6f593cb258a0886d3b0d57a225ad9
blubiszirp <frank-jordi@hotmail.com>
2017-01-16 10:11:56 +0100

    better sadness

--- scripts/animations/sad2.js
+++ scripts/animations/sad2.js
@@ -1,61 +1,53 @@
 define(function(){
 const duration = 100;
 return [
-	{
-        duration: duration * 10,
-        image: [
-			"(\\_/)",
-			"(o_o)",
-			"(> <)"
-        ]
-    },
-    {
-        duration: duration * 30,
-        image: [
+  {
+    duration: duration * 30,
+    image: [
 			"(\\_/)",
 			"(._.)",
 			"(> <)"
-        ]
-    },
+    ]
+  },
 	{
-        duration: duration * 20,
-        image: [
+    duration: duration * 20,
+    image: [
 			"(\\_/)",
 			"( ._)",
 			"(> <)"
-        ]
-    },
+    ]
+  },
 	{
-        duration: duration,
-        image: [
+    duration: duration,
+    image: [
 			"(\\_/)",
 			"( -_)",
 			"(> <)"
-        ]
-    },
+    ]
+  },
 	{
-        duration: duration * 20,
-        image: [
+    duration: duration * 20,
+    image: [
 			"(\\_/)",
 			"( ._)",
 			"(> <)"
-        ]
-    },
+    ]
+  },
 	{
-        duration: duration * 20,
-        image: [
+    duration: duration * 20,
+    image: [
 			"(\\_/)",
 			"(._.)",
 			"(> <)"
-        ]
-    },
+    ]
+  },
 	{
-        duration: duration * 30,
-        image: [
+    duration: duration * 30,
+    image: [
 			"(\\_/)",
 			"(T_T)",
 			"(> <)"
-        ]
-    },
+    ]
+  },
 ]
 });
